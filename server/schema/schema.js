1c1b233f8c3c3b74882a3df4b32d3869d9857453
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2019-07-28 00:43:26 +0530

    end points for all data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -72,6 +72,18 @@ const RootQuery = new GraphQLObjectType({   // end points of data graph
             resolve(parent, args){
                 return _.find(authors, { id: args.id })
             }
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent, args){
+                return books
+            }
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent, args){
+                return authors
+            }
         }
     }
 })

d413cf7cc5c3303ed483ef3fb0a1a32bd872a857
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2019-07-28 17:14:42 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const schema = require('./schema/schema')
 
 const app = express()
 
-mongoose.connect('mongodb://tanay:test123@ds255857.mlab.com:55857/library')
+mongoose.connect('mongodb://tanay:test123@ds255857.mlab.com:55857/library', {
+    useNewUrlParser: true
+})
 mongoose.connection.once('open', () => {
     console.log('connected to database')
 })
 
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     graphiql: true
